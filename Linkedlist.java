0a252291ec940a489e950651d5c21519962d946e
priyasha1808 <147809315+priyasha1808@users.noreply.github.com>
2024-06-05 12:43:40 +0530

    Linkedlist.java

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ !!!!!!!!!! THE INDEX AND POSITION MAY VARY. SMALL CHANGES IN LOOP IS ENOUGH TO CORRECT !!!!!!!!!!!!
  */
 package dsa;
 import java.util.*;
